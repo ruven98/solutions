c1dff1232c389d30325f36b6af66e312031f02b0
Ruven Micheel <ruven.micheel@web.de>
2024-09-10 13:55:01 +0200

    Added my solutions for tabular data

--- codingcoursemodule.py
+++ codingcoursemodule.py
@@ -1,6 +1,11 @@
 def mean(x):
     return sum(x) / len(x)
 
+
 def median(x):
-    center = len(x)//2
-    return sorted(x)[center] if len(x)%2!=0 else mean(sorted(x)[center-1:center+1])
+    center = len(x) // 2
+    return (
+        sorted(x)[center]
+        if len(x) % 2 != 0
+        else mean(sorted(x)[center - 1 : center + 1])
+    )
